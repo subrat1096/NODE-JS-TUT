08553c9e68e643e0f3b160ab002f190d95798854
Subrat Kumar Behera <148032888+codexpsk@users.noreply.github.com>
2023-10-29 08:57:57 +0530

    custom module

---
+++ 06_Custom Modules/app.js
@@ -0,0 +1,7 @@
+const { inc, dec, getCount } = require("./index");
+
+inc();
+inc();
+inc();
+
+console.log(`the count is ${getCount()}`);
---
+++ 06_Custom Modules/index.js
@@ -0,0 +1,13 @@
+let count = 0;
+const inc = () => ++count;
+const dec = () => --count;
+const getCount = () => count;
+
+module.exports = {
+  anything: true,
+  who: "Subrat Kumar Behera",
+  count,
+  inc,
+  dec,
+  getCount,
+};
